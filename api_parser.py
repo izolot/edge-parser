b023bed58645ca2b0a87616f6e94b6083deb7f3f
izolot <izolot>
2019-06-14 17:49:01 +0300

    add some functional for parse folder

--- api_parser.py
+++ api_parser.py
@@ -1,21 +1,29 @@
 import parser
-import datetime
 from flask import Flask
 from flask import jsonify
 from flask import request
 from flask import send_file
 from flask_autoindex import AutoIndex
 
-app = Flask(__name__)
-AutoIndex(app, browse_root='mnt')
+ROOT_PATH = "mnt"
 
+app = Flask(__name__)
+AutoIndex(app, browse_root=ROOT_PATH)
+pars = parser.Parser(ROOT_PATH)
+pars.init_camera_folders()
 
 @app.route('/events', methods = ['GET'])
 def get_events_by_time():
     start = request.args.get('start')
     end = request.args.get('end')
-    pars = parser.Parser('mnt/edge1/local_arch/')
-    return jsonify({'result': pars.search_by_time(start, end)})
+    uuid = request.args.get('uuid')
+    return jsonify({'result': pars.search_by_time(start, end, uuid)})
+
+@app.route('/excel', methods = ['GET'])
+def get_excel():
+    return send_file(pars.create_excel())
+
+
 
 
 if __name__ == '__main__':
--- parser.py
+++ parser.py
@@ -1,39 +1,73 @@
-
 import json
 import os
 import xlsxwriter
-import time
 import glob
+import datetime
+import pathlib
 
 
-class Parser:
-    def __init__(self, root_path:str):
+class Parser(object):
+    def __init__(self, root_path: str):
         self.root_path = root_path
+        self.events = []
+        self.camera_uuids = {}
 
-    def search_by_time(self, start: str, end: str) -> dict:
-        start_time = format_date(start)
-        end_time = format_date(end)
-        path = self.root_path + start_time['day_path']
-        args_dicts = []
+    def init_camera_folders(self)-> dict:
+        for root, _, files in os.walk(self.root_path):
+            for file in files:
+                if '.json' in file:
+                    with open(os.path.join(root,file)) as args:
+                        current_uuid = json.load(args)['origin']['uuid']
+                        if current_uuid not in self.camera_uuids.keys():
+                            chunk_path = root.split('/')
+                            self.camera_uuids[current_uuid] = '/'.join(chunk_path[0:3])
+
+    def search_by_time(self, start: str, end: str, uuid: str) -> dict:
+        camera_path = self.get_path_by_uuid(uuid)
+        start_time = self.format_date(start)
+        end_time = self.format_date(end)
+        path = camera_path + os.sep + start_time['day_path']
         for directory in sorted(os.listdir(path)):
             if start_time['hour'] <= int(directory) and int(directory) <= end_time['hour']:
                 pattern = directory + '/*/*/*.json'
                 filenames = glob.glob(os.path.join(path, pattern))
-                for filename in filenames:
+                for filename in sorted(filenames):
                     with open(filename) as args:
                         json_dict = json.load(args)
-                        args_dict = dict()
-                        args_dict['path'] = filename[:-9]
-                        args_dict['uuid'] = json_dict['uuid']
-                        args_dict['time'] = json_dict['time']
-                        args_dict['location'] = json_dict['origin']['location']['place']
-                        args_dict['images'] = json_dict['measurements'][0]['waypoints']['best']['images']
-                        args_dict['plate_text'] = json_dict['measurements'][0]['waypoints']['best']['vehicle']['license-plates'][0]['text']['ucode']
-                        args_dicts.append(args_dict)
-        print("count json events: ", len(args_dicts))
-        return args_dicts
-    
-    def format_date(self,date: str) -> dict:
+                        if json_dict['origin']['uuid'] == uuid:
+                            args_dict = dict()
+                            args_dict['path'] = filename[:-9]
+                            #camera uuid
+                            args_dict['uuid'] = json_dict['origin']['uuid']
+                            args_dict['time'] = json_dict['time']
+                            args_dict['location'] = json_dict['origin']['location']['place']
+                            images = json_dict['measurements'][0]['waypoints']['best']['images']
+                            args_dict['images'] = {
+                                "vehicle": images['vehicle']['name'],
+                                "scene": images['scene']['name'],
+                                "thumb": images['thumb']['name'],
+                                "plate": images['license-plates'][0]['name']
+                            }
+                            vehicle = json_dict['measurements'][0]['waypoints']['best']['vehicle']
+                            if "type" in vehicle.keys():
+                                args_dict['vehicle'] = {
+                                    "class": vehicle['type']['info'][0]['class'],
+                                    "make": vehicle['type']['make'],
+                                    "model": vehicle['type']['model'],
+                                    "weight": vehicle['type']['weight']
+                                }
+                            else:
+                                args_dict['vehicle'] = {
+                                    "class": "",
+                                    "make": "",
+                                    "model": "",
+                                    "weight": ""
+                                }
+                            args_dict['vehicle']['plate-text'] = vehicle['license-plates'][0]['text']['ucode']
+                            self.events.append(args_dict)
+        print("count json events: ", len(self.events))
+
+    def format_date(self, date: str) -> dict:
         datestr = date.split('.')[0]
         tmp = datetime.datetime.strptime(datestr, "%Y-%m-%dT%H:%M:%S")
         day_path = "{:4d}/{:02d}/{:02d}".format(tmp.year, tmp.month, tmp.day)
@@ -44,42 +78,49 @@ class Parser:
         }
         return dict_date
 
-def create_excel(data: dict) -> int:
-    workbook = xlsxwriter.Workbook('otchet.xlsx')
-    worksheet = workbook.add_worksheet()
-    excel_format = workbook.add_format(
-        {'align': 'center', 'valign': 'vcenter', 'text_wrap': True})
+    def create_excel(self) -> int:
+        os.curdir(self.root_path)
+        if not os.path.exists("xlsx"):
+            os.makedirs("xlsx")
+        now = datetime.datetime.now()
+        xsls_name = now.date() + '.xlsx'    
+        workbook = xlsxwriter.Workbook(root_path + '/xlsx/' + xsls_name)
+        worksheet = workbook.add_worksheet()
+        excel_format = workbook.add_format(
+            {'align': 'center', 'valign': 'vcenter', 'text_wrap': True})
 
-    worksheet.set_column('A:E', 31, excel_format)
-    worksheet.write('A1', 'Фото машины')
-    worksheet.write('B1', 'Фото ГРЗ')
-    worksheet.write('C1', 'Камера')
-    worksheet.write('D1', 'Дата и время события')
-    worksheet.write('E1', 'Распознанный номер')
+        worksheet.set_column('A:E', 31, excel_format)
+        worksheet.write('A1', 'Фото машины')
+        worksheet.write('B1', 'Фото ГРЗ')
+        worksheet.write('C1', 'Камера')
+        worksheet.write('D1', 'Дата и время события')
+        worksheet.write('E1', 'Распознанный номер')
 
-    row = 1
-    for el in data:
-        worksheet.set_row(row, 170, excel_format)
-        worksheet.insert_image(
-            row, 0, os.path.join(el['path'], el['images']['thumb']['name']),
-            {'object_position': 3})
-        worksheet.insert_image(
-            row, 1, os.path.join(
-                el['path'], el['images']['license-plates'][0]['name']),
-            {'object_position': 3, 'x_offset': 20, 'y_offset': 20,
-                'x_scale': 1.5, 'y_scale': 1.5})
-        worksheet.write(row, 2, el['location'])
-        worksheet.write(row, 3, el['time'])
-        worksheet.write(row, 4, el['plate_text'])
-        row += 1
+        row = 1
+        for el in self.events:
+            worksheet.set_row(row, 170, excel_format)
+            worksheet.insert_image(
+                row, 0, os.path.join(el['path'], el['images']['thumb']['name']),
+                {'object_position': 3})
+            worksheet.insert_image(
+                row, 1, os.path.join(
+                    el['path'], el['images']['license-plates'][0]['name']),
+                {'object_position': 3, 'x_offset': 20, 'y_offset': 20,
+                    'x_scale': 1.5, 'y_scale': 1.5})
+            worksheet.write(row, 2, el['location'])
+            worksheet.write(row, 3, el['time'])
+            worksheet.write(row, 4, el['plate_text'])
+            row += 1
 
-    workbook.close()
-    print("Done!")
+        workbook.close()
+        print("Done!")
+
+    def get_path_by_uuid(self, uuid) -> str:
+        return self.camera_uuids[uuid]
+# for test
 
 
 
-
-#for test
 def counter_jsonfile(path: str) -> int:
     count = 0
     for _, _, files in os.walk(path):
@@ -88,13 +129,4 @@ def counter_jsonfile(path: str) -> int:
                 count += 1
 
     return count
-
-
-
-
-
-
-
-
-
 
