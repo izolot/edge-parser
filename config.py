62f5af33c67a719c23a5ef686d87cc836ff6f6c7
izolot <izolot>
2019-07-03 15:14:44 +0300

    parameter for save excel

--- config.py
+++ config.py
@@ -2,9 +2,12 @@ class Config(object):
     DEBUG = False
     TESTING = False
     ARCHIVE_PATH = ""
+    EXCEL_LIMIT_ROW = 1000
 
 class DevelopmentConfig(Config):
     DEBUG = True
 
 class TestingConfig(Config):
     TESTING = True 
+    ARCHIVE_PATH = "/test"
+    
